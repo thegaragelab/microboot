9d4e823831ed43076eb9ca316df8fc856e751081
shane <shane@thegaragelab.com>
2014-03-31 18:05:47 +1000

    Updated Makefile and some slight changes to the wait time for the bootloader.

--- atmega/bootloaders/babyboot/avrboot.c
+++ atmega/bootloaders/babyboot/avrboot.c
@@ -84,7 +84,7 @@
 /* 20070626: hacked by David A. Mellis to decrease waiting time for auto-reset */
 /* set the waiting time for the bootloader */
 // How long to wait for boot loader, at 8MHz 2000000 will be about 1s
-#define MAX_TIME_COUNT (F_CPU>>3)
+#define MAX_TIME_COUNT 6000000
 
 /* 20070707: hacked by David A. Mellis - after this many errors give up and launch application */
 #define MAX_ERROR_COUNT 5
@@ -93,7 +93,7 @@
 /* 20060803: hacked by DojoCorp */
 // SWG: Expect the baud rate on the command line, use a default if not.
 #ifndef BAUD_RATE
-#  define BAUD_RATE   19200
+#  define BAUD_RATE   38400
 #endif
 
 /* SW_MAJOR and MINOR needs to be updated from time to time to avoid warning message from AVR Studio */
