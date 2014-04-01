dbd45ea635bd3c1ca567b03cb066a1133390fd34
Shane Gough <shane@thegaragelab.com>
2014-04-01 20:55:44 +1000

    Added code to write to flash, need to make some adjustments to support multiple processors.

--- atmega/microboot.c
+++ atmega/microboot.c
@@ -34,6 +34,9 @@
 #define CPU_TYPE         0x01
 #define CPU_MODEL        0x02
 
+// Size of flash pages in memory
+#define PAGE_SIZE	 0x20
+
 uint8_t g_buffer[BUFFER_SIZE];
 
 //---------------------------------------------------------------------------
@@ -68,6 +71,69 @@ inline uint16_t checksum(uint16_t total, uint8_t data) {
 /** Write the current buffer contents into flash
  */
 bool writeFlash() {
+  while(bit_is_set(EECR,EEWE)); // Wait for previous EEPROM writes to complete
+  asm volatile(
+    "  clr        r17                   \n\t"   //page_word_count
+    "  lds        r30,g_buffer          \n\t"   //Address of FLASH location (in bytes)
+    "  lds        r31,g_buffer+1        \n\t"
+    "  ldi        r28,lo8(g_buffer + 2) \n\t"   //Start of buffer array in RAM
+    "  ldi        r29,hi8(g_buffer + 2) \n\t"
+    "  ldi        r24,0                 \n\t"     //Length of data to be written (in bytes)
+    "  ldi        r25,%[data_size]      \n\t"
+    "  rjmp       length_loop           \n\t"
+    "  adiw       r24,1                 \n\t"
+    "length_loop:                       \n\t"   //Main loop, repeat for number of words in block
+    "  cpi        r17,0x00              \n\t"   //If page_word_count=0 then erase page
+    "  brne       no_page_erase         \n\t"
+    "  rcall      wait_spm              \n\t"
+    "  ldi        r16,0x03              \n\t"   //Erase page pointed to by Z
+    "  sts        %0,r16                \n\t"
+    "  spm                              \n\t"
+    "  rcall      wait_spm              \n\t"
+    "  ldi        r16,0x11              \n\t"   //Re-enable RWW section
+    "  sts        %0,r16                \n\t"
+    "  spm                              \n\t"
+    "no_page_erase:                     \n\t"
+    "  ld         r0,Y+                 \n\t"   //Write 2 bytes into page buffer
+    "  ld         r1,Y+                 \n\t"
+    "  rcall      wait_spm              \n\t"
+    "  ldi        r16,0x01              \n\t"   //Load r0,r1 into FLASH page buffer
+    "  sts        %0,r16                \n\t"
+    "  spm                              \n\t"
+    "  inc        r17                   \n\t"   //page_word_count++
+    "  cpi        r17,%1                \n\t"
+    "  brlo       same_page             \n\t"   //Still same page in FLASH
+    "write_page:                        \n\t"
+    "  clr        r17                   \n\t"   //New page, write current one first
+    "  rcall      wait_spm              \n\t"
+    "  ldi        r16,0x05              \n\t"   //Write page pointed to by Z
+    "  sts        %0,r16                \n\t"
+    "  spm                              \n\t"
+    "  rcall      wait_spm              \n\t"
+    "  ldi        r16,0x11              \n\t"   //Re-enable RWW section
+    "  sts        %0,r16                \n\t"
+    "  spm                              \n\t"
+    "same_page:                         \n\t"
+    "  adiw       r30,2                 \n\t"   //Next word in FLASH
+    "  sbiw       r24,2                 \n\t"   //length-2
+    "  breq       final_write           \n\t"   //Finished
+    "  rjmp       length_loop           \n\t"
+    "wait_spm:                          \n\t"
+    "  lds        r16,%0                \n\t"   //Wait for previous spm to complete
+    "  andi       r16,1                 \n\t"
+    "  cpi        r16,1                 \n\t"
+    "  breq       wait_spm              \n\t"
+    "  ret                              \n\t"
+    "final_write:                       \n\t"
+    "  cpi        r17,0                 \n\t"
+    "  breq       block_done            \n\t"
+    "  adiw       r24,2                 \n\t"   //length+2, fool above check on length after short page write
+    "  rjmp       write_page            \n\t"
+    "block_done:                        \n\t"
+    "  clr        __zero_reg__          \n\t"   //restore zero register
+    : "=m" (SPMCR)
+    : "M" (PAGE_SIZE), [data_size] "I" (DATA_SIZE)
+    : "r0","r16","r17","r24","r25","r28","r29","r30","r31");
   return true;
   }
 
