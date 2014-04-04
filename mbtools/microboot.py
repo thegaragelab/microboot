ab9cae31840f71ea0e1316880b6682a4564599d9
Shane Gough <shane@thegaragelab.com>
2014-04-04 15:18:06 +1000

    Fleshing out the main operations (read, write, verify)

--- mbtools/microboot.py
+++ mbtools/microboot.py
@@ -22,14 +22,25 @@ CHIPLIST = {
   "atmega168": ( 0x01, 0x04, 0x10,    0x0000, 0xFFFF ),
   }
 
+#--- These chips have single bit software UARTS, account for echo
+SOFTUART = (
+  "attiny85",
+  )
+
 #--- Protocol constants
 CMD_READ      = 'R'    # Character for the READ command
 CMD_WRITE     = 'W'    # Character for the WRITE command
 CMD_QUERY     = '?'    # Character for the QUERY command
 CMD_RESET     = '!'    # Character for the RESET command
+STATUS_OK     = '+'    # Character for OK status
+STATUS_FAIL   = '-'    # Character for FAIL status
 EOL           = '\n'   # Character to use for line termination
 DATA_SIZE     = 16     # Number of data bytes in a packet
 CHECKSUM_SEED = 0x5050 # Seed value for calculating checksum
+
+#--- Default values
+DEFAULT_SPEED   = 57600  # Baud rate for serial connection
+DEFAULT_RETRIES = 3      # Number of retries per command
 
 #----------------------------------------------------------------------------
 # Public API and classes
@@ -58,7 +69,11 @@ class Microboot:
   """
 
   def __init__(self):
-    pass
+    # Set initial state
+    self.deviceName = None
+    self.deviceInfo = None
+    self.bootInfo = None
+    self.serial = None
 
   #--------------------------------------------------------------------------
   # Helper methods
@@ -72,11 +87,30 @@ class Microboot:
         @return the parameter as a sequence.
     """
     if not isinstance(data, collections.Sequence):
-      if typeof(data) == str:
+      if type(data) == str:
         data = [ chr(x) for x in data ]
       else:
         data = ( data, )
     return data
+
+  def verifyRange(self, data, offset = 0, length = -1):
+    """ Verify that the sequence is large enough
+
+        @param data the sequence of values to verify
+        @param offset the offset into the array to start reading data
+        @param length the number of bytes. If this value is less than zero the
+                      remainder of the array will be used.
+
+        @throws MicrobootException if the sequence is not large enough.
+    """
+    # Rationalise the length
+    if length < 0:
+      length = len(data) - offset
+    # Make sure there is enough data
+    if offset > len(data):
+      raise MicrobootException("Data array is not of sufficient size (%d > %d)." % (offset, len(data)))
+    if (length + offset) > len(data):
+      raise MicrobootException("Data array is not of sufficient size (%d > %d)." % (length + offset, len(data)))
 
   def checksum(self, total, data, offset = 0, length = -1):
     """ Add the given data to the checksum
@@ -98,10 +132,7 @@ class Microboot:
     if length < 0:
       length = len(data) - offset
     # Make sure there is enough data
-    if offset > len(data):
-      raise MicrobootException("Data array is not of sufficient size (%d > %d)." % (offset, len(data)))
-    if (length + offset) > len(data):
-      raise MicrobootException("Data array is not of sufficient size (%d > %d)." % (length + offset, len(data)))
+    self.verifyRange(data, offset, length)
     # Now update the checksum
     for val in data:
       val = int(val)
@@ -116,6 +147,8 @@ class Microboot:
     """
     # Make sure we have a sequence
     data = self.makeArray(data)
+    # Make sure there is enough data
+    self.verifyRange(data, offset, DATA_SIZE)
     # Calculate the checksum first
     check = CHECKSUM_SEED
     check = self.checksum(check, ((address >> 8) & 0xFF, address & 0xFF))
@@ -131,11 +164,83 @@ class Microboot:
       EOL
       )
 
+  def createReadCommand(self, address):
+    """ Create the 'READ' command string
+    """
+    # Calculate the checksum first
+    check = CHECKSUM_SEED
+    check = self.checksum(check, ((address >> 8) & 0xFF, address & 0xFF))
+    # Now build up the string and return it
+    return "%c%02X%02X%02X%02X%c" % (
+      CMD_READ,
+      (address >> 8) & 0xFF, # Address high byte
+      address & 0xFF, # Address low byte
+      (check >> 8) & 0xFF, # Checksum high byte
+      check & 0xFF, # Checksum low byte
+      EOL
+      )
+
+  def parseData(self, response):
+    """ Parse a response string into a sequence of bytes.
+    """
+    values = list()
+    for index in range((len(response) - 2) / 2):
+      values.append(int(response[1 + (2 * index):3 + (2 * index)], 16))
+    # Verify checksum
+    check = CHECKSUM_SEED
+    for val in values[:-2]:
+      check = self.checksum(check, val)
+    if (((check >> 8) & 0xFF) <> values[len(values) - 2]) or ((check & 0xFF) <> values[len(values) - 1]):
+      raise MicrobootException("Checksum does not match - expected 0x%04X, got 0x%04X." % (check, (values[len(values) - 2] << 8) | values[len(values) - 1]))
+    return values
+
+  def sendCommand(self, command):
+    """ Send a command string and read the response
+    """
+    attempts = 0
+    while attempts < DEFAULT_RETRIES:
+      # Send the command
+      if self.deviceName in SOFTUART:
+        # Consume what we send - it gets echoed back
+        for ch in command:
+          self.serial.write(ch)
+          rcvd = self.serial.read(1)
+          if rcvd <> ch:
+            raise MicrobootException("Unexpected response on serial port - got '%c', expected '%c'." % (rcvd, ch))
+      else:
+        written = 0
+        while written < len(command):
+          written = written + self.serial.write(command[written:])
+      # Read the response
+      response = ""
+      while True:
+        data = self.serial.read(1)
+        if len(data) == 0:
+          break
+        response = response + chr(data[0])
+        if response[:-1] == EOL:
+          break
+      # Do some basic validation on the response
+      if len(response) >= 2:
+        if response[:-1] == EOL:
+          if response[0] == STATUS_OK:
+            if len(response) == 2: # OK only
+              return ()
+            # Turn it into a sequence of bytes (and verify checksum)
+            try:
+              return self.parseData(response)
+            except:
+              pass
+      # Try again
+      attempts = attempts + 1
+    # At this stage, we've failed
+    raise MicrobootException("Failed to send command '%c', no response or command failed." % command[0])
+
   #--------------------------------------------------------------------------
   # Public methods
   #--------------------------------------------------------------------------
 
-  def connect(self, port, device):
+  def connect(self, device, port, speed = DEFAULT_SPEED):
     """ Connect to the device attached to the given port.
 
         This will fail if there is a communication error on the port (or the
@@ -156,7 +261,7 @@ class Microboot:
 
         @return True if a connection is currently active.
     """
-    return False
+    return self.serial is not None
 
   def disconnect(self):
     """ Disconnect from the device.
@@ -164,10 +269,20 @@ class Microboot:
         Close the connection to the device (and the underlying serial port). If
         the connection is not established yet this method will have no effect.
     """
-    pass
+    if self.connected():
+      # Close the connection
+      try:
+        self.serial.close()
+      except:
+        pass # Do nothing
+    # Restore initial state
+    self.deviceName = None
+    self.deviceInfo = None
+    self.bootInfo = None
+    self.serial = None
 
   def getInfo(self):
-    """ Get the device information tuple.
+    """ Get the bootloader information tuple.
 
         This method queries the device for it's information block which includes
         processor family, processor type and protocol version.
@@ -175,9 +290,18 @@ class Microboot:
         @return a 3-tuple of integers consisting of family, model and protocol
                 or None if a connection has not been established.
     """
-    return None
+    if not self.connected():
+      return None
+    # Return cached values if we have them
+    if self.bootInfo is None:
+      # Query the device for the information
+      response = self.sendCommand("%c%c" % (CMD_QUERY, EOL))
+      if len(response) <> 5: # 3 data bytes, 2 checksum bytes
+        raise MicrobootException("Invalid response from device.")
+      self.bootInfo = tuple(response[:3])
+    return self.bootInfo
 
-  def read(self, start, length):
+  def read(self, start, length, callback = None):
     """ Read data from the flash memory
 
         Query the chip for the contents of flash starting at 'start' for 'length'
@@ -188,6 +312,11 @@ class Microboot:
         @param length the number of bytes to read. Must be a positive integer
                       greater than zero such that start + length is in the valid
                       address range for the device.
+        @param callback optional callback function used to report progress. It
+                        will be passed two integer values - the number of bytes
+                        that have been transferred and the number of bytes
+                        requested.
+
 
         @return an array of bytes representing the contents of the flash memory
                 or None if the device is not connected.
@@ -195,9 +324,36 @@ class Microboot:
         @throws MicrobootException if the address is out of range or a
                 communication error occurs.
     """
-    return None
+    if not self.connected():
+      return None
+    # Check parameters
+    if (start < 0) or (length < 0):
+      raise MicrobootException("Parameters out of range - must be positive integers.")
+    # Determine the number of bytes to read (must be multiple of DATA_SIZE)
+    alength = length
+    if (alength % DATA_SIZE) <> 0:
+      alength = (int(length / DATA_SIZE) + 1) * DATA_SIZE
+    # Make sure the addresses are valid for the device
+    chipInfo = CHIPLIST[self.deviceName]
+    if (start < chipInfo[3]) or (start > chipInfo[4]) or ((start + length) > chipInfo[4]):
+      raise MicrobootException("Address out of range for device - %04X:%04X" % (chipInfo[3], chipInfo[4]))
+    # Now do the read
+    read = 0
+    address = start
+    result = list()
+    while read < alength:
+      response = self.sendCommand(self.createReadCommand(address))
+      if len(response) <> (DATA_SIZE + 4): # Address + data + checksum
+        raise MicrobootException("Invalid response from device.")
+      result.extend(response[2:-2])
+      address = address + DATA_SIZE
+      read = read + DATA_SIZE
+      if callback is not None:
+        callback(min(read, length), length)
+    # All done
+    return result[:length]
 
-  def write(self, start, length, data):
+  def write(self, start, length, data, callback = None):
     """ Write data to the flash memory
 
         Replace the contents of the flash memory with the given bytes starting
@@ -210,11 +366,77 @@ class Microboot:
                       address range for the device.
         @param data an array of bytes to be written to the flash. The length of
                     the must be at least 'length' bytes.
+        @param callback optional callback function used to report progress. It
+                        will be passed two integer values - the number of bytes
+                        that have been transferred and the number of bytes
+                        requested.
 
         @throws MicrobootException if the address is out of range or a
                 communication error occurs.
     """
-    pass
+    if not self.connected():
+      return
+    # Check parameters
+    if (start < 0) or (length < 0):
+      raise MicrobootException("Parameters out of range - must be positive integers.")
+    # Make sure we have an array of data
+    data = self.makeArray(data)
+    self.verifyRange(data, 0, length)
+    # Determine the number of bytes to write (must be multiple of DATA_SIZE)
+    alength = length
+    if (alength % DATA_SIZE) <> 0:
+      alength = (int(length / DATA_SIZE) + 1) * DATA_SIZE
+    # Make sure the addresses are valid for the device
+    chipInfo = CHIPLIST[self.deviceName]
+    if (start < chipInfo[3]) or (start > chipInfo[4]) or ((start + length) > chipInfo[4]):
+      raise MicrobootException("Address out of range for device - %04X:%04X" % (chipInfo[3], chipInfo[4]))
+    # If we have been given less than DATA_SIZE bytes to write, pad with what
+    # is already there
+    if alength <> length:
+      current = self.read(start + alength - DATA_SIZE, DATA_SIZE)
+      data.extend(current[DATA_SIZE - (alegnth - length):])
+    # Now write the data
+    written = 0
+    address = start
+    result = list()
+    while read < alength:
+      response = self.sendCommand(self.createWriteCommand(address, data, written, DATA_SIZE))
+      if len(response) <> 0: # Should be no data in response
+        raise MicrobootException("Invalid response from device.")
+      result.extend(response[2:-2])
+      address = address + DATA_SIZE
+      written = written + DATA_SIZE
+      if callback is not None:
+        callback(min(written, length), length)
+
+  def verify(self, start, length, data, callback = None):
+    """ Verify data in the flash memory
+
+        Verify the contents of the flash memory with the given bytes starting
+        at address 'start' for 'length' bytes.
+
+        @param start the start address to read from. Must be a positive integer
+                     in the allowable address range for the device.
+        @param length the number of bytes to read. Must be a positive integer
+                      greater than zero such that start + length is in the valid
+                      address range for the device.
+        @param data an array of bytes to be verified against. The length of
+                    the must be at least 'length' bytes.
+        @param callback optional callback function used to report progress. It
+                        will be passed two integer values - the number of bytes
+                        that have been transferred and the number of bytes
+                        requested.
+
+        @throws MicrobootException if the address is out of range or a
+                communication error occurs.
+    """
+    # Read the data
+    current = self.read(start, length, callback = callback)
+    # Do the verification
+    for index in range(length):
+      if data[index] <> current[index]:
+        raise MicrobootException("Verification failed. Value at %04X is 0x%02X, expected 0x%02X." % (start + index, current[index], data[index]))
+    # At this point it all worked.
 
   def reset(self):
     """ Reset the device.
@@ -225,8 +447,5 @@ class Microboot:
 
 # Make sure we are just being imported
 if __name__ == "__main__":
-  #print "Error: This file is a support library, do not run it directly."
-  #exit(1)
-  mb = Microboot()
-  print mb.createWriteCommand(0x1000, (0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f))
-
+  print "Error: This file is a support library, do not run it directly."
+  exit(1)
