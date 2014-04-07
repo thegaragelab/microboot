8f948a8b33267ab77c990c4833b8a71b123362ae
Shane Gough <shane@thegaragelab.com>
2014-04-07 14:34:03 +1000

    Fleshing out the flash utility.

--- mbtools/mbflash.py
+++ mbtools/mbflash.py
@@ -5,6 +5,8 @@
 # Utility to flash a microcontroller over serial connections.
 #----------------------------------------------------------------------------
 import sys
+from os.path import splitext, basename, exists
+from intelhex import IntelHex
 from microboot import Microboot, MicrobootException
 
 #--- Banner and usage information
@@ -13,15 +15,104 @@ mbflash.py - Microboot/Microboard System Flashing Utility
 Copyright (c) 2014, The Garage Lab. All Rights Reserved.
 """
 
+USAGE = """
+Usage:
+
+  %s options filename
+
+Options:
+  -d,--device name  Specify the expected device, eg: attiny85,atmega8. This
+                    is required.
+  -p,--port name    Specify the name of the serial port to use for communication.
+                    If not specified the port /dev/ttyUSB0 will be used.
+  --log             Log all communications to the file 'transfer.log'
+"""
+
+#--- Communications log
+g_logFile = None
+
 #----------------------------------------------------------------------------
 # Helper functions
 #----------------------------------------------------------------------------
+
 def showUsage():
+  print USAGE.strip() % basename(sys.argv[0])
   exit(1)
+
+def logFunction(send, recv):
+  g_logFile.write(">" + send)
+  g_logFile.write("<" + recv)
 
 #----------------------------------------------------------------------------
 # Main program
 #----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-  print BANNER.strip()
+  print BANNER.strip() + "\n"
+  # Set up defaults
+  device = None
+  port = "/dev/ttyUSB0"
+  filename = None
+  # Process command line arguments
+  index = 1
+  while index < len(sys.argv):
+    arg = sys.argv[index]
+    if (arg == "--device") or (arg == "-d"):
+      index = index + 1
+      device = sys.argv[index]
+    elif (arg == "--port") or (arg == "-p"):
+      index = index + 1
+      port = sys.argv[index]
+    elif (arg == "--log"):
+      g_logfile = open("transfer.log", "w")
+    else:
+      filename = arg
+      index = index + 1
+      if index <> len(sys.argv):
+        print "Error: Filename must be the last parameter.\n"
+        showUsage()
+    index = index + 1
+  # Rationalise parameters
+  if device is None:
+    print "Error: You must specify a device.\n"
+    showUsage()
+  if filename is None:
+    print "Error: You must specify a filename.\n"
+    showUsage()
+  # Add default extension to filename if the unadorned version does not exist
+  if not exists(filename):
+    name, ext = splitext(filename)
+    if ext == "":
+      filename = name + ".hex"
+  if not exists(filename):
+    print "Error: You must specify a filename.\n"
+    showUsage()
+  # Set up the device interface
+  mb = Microboot()
+  info = mb.getDeviceInfo(device)
+  if info is None:
+    print "Unsupported device type '%s'." % device
+    exit(1)
+  # Set up logging if requested
+  if g_logFile is not None:
+    mb.logger = logFunction
+  # Load the HEX file
+  hexfile = IntelHex()
+  hexfile.fromfile(filename, format='hex')
+  start = hexfile.minaddr()
+  length = (hexfile.maxaddr() - start) + 1
+  data = list()
+  for index in range(length):
+    data.append(hexfile[start + index])
+  print "Writing %d bytes (%04X:%04X) from file '%s'." % (length, start, start + length - 1, filename)
+  print "Target is a '%s' on port '%s'." % (device, port)
+  # Write the data
+  try:
+    mb.connect(device, port)
+  except Exception, ex:
+    print "Error: Could not connect to device, error message is:"
+    print "       " + str(ex)
+    exit(1)
+  mb.write(start, length, data)
+  mb.close()
+
