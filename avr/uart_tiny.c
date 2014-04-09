7cb61c063d812975350e5756fe95077ca979e9a4
shane <shane@thegaragelab.com>
2014-04-09 11:39:42 +1000

    Fixed bug in the assembly code for uartSend().

--- avr/uart_tiny.c
+++ avr/uart_tiny.c
@@ -19,6 +19,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "hardware.h"
 #include "microboot.h"
 
@@ -105,10 +106,10 @@ void uartSend(char ch) {
     "  ldi r28, %[txdelay]              \n\t"
     "TxLoop:                            \n\t"
     // 8 cycle loop + delay - total = 7 + 3*r22
-    "  mov r19, r22                     \n\t"
+    "  mov r29, r28                     \n\t"
     "TxDelay:                           \n\t"
     // delay (3 cycle * delayCount) - 1
-    "  dec r19                          \n\t"
+    "  dec r29                          \n\t"
     "  brne TxDelay                     \n\t"
     "  bst %[ch], 0                     \n\t"
     "  bld r0, %[uart_pin]              \n\t"
@@ -122,5 +123,7 @@ void uartSend(char ch) {
       [txdelay] "I" (TXDELAY),
       [ch] "r" (ch)
     : "r0","r28","r29","r30");
+  // Change back to idle state
+  UART_PORT |= (1 << UART_PIN);
   }
 
