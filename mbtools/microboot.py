8941a3a018fd3625d1cd01bb5a8b2fe3b566576f
Shane Gough <shane@thegaragelab.com>
2014-04-14 18:09:28 +1000

    Fixed off by one error when calculate maximum address range in writes.

--- mbtools/microboot.py
+++ mbtools/microboot.py
@@ -466,7 +466,7 @@ class Microboot:
       alength = (int(length / self.DATA_SIZE) + 1) * self.DATA_SIZE
     # Make sure the addresses are valid for the device
     chipInfo = CHIPLIST[self.deviceName]
-    if (start < chipInfo[3]) or (start > chipInfo[4]) or ((start + length) > chipInfo[4]):
+    if (start < chipInfo[3]) or (start > chipInfo[4]) or ((start + length - 1) > chipInfo[4]):
       raise MicrobootException("Address out of range for device - %04X:%04X" % (chipInfo[3], chipInfo[4]))
     # If we have been given less than self.DATA_SIZE bytes to write, pad with what
     # is already there
